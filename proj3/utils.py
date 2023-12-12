d52ba88c87bdc7e2a6545df3ff84f18fdd443fd4
LuoXubo <luoxubo@hotmail.com>
2023-12-12 21:35:01 +0800

    update machine learning

--- proj3/utils.py
+++ proj3/utils.py
@@ -141,3 +141,98 @@ def Trainer (train_set, test_set, net, epochs, criterion, optimizer) :
           % (epoch, epochs, losses[-1], train_acc[-1], test_acc[-1]))
   torch.save (net.state_dict (), 'resnet18.pth')
   return losses, train_acc, test_acc, cnt
+
+# functions used in machine learning methods
+def cluster_acc(y_true, y_pred):
+    y_true = np.array(y_true).astype(np.int64)
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+    ind = linear_sum_assignment(w.max() - w)
+    ind = np.asarray(ind)
+    ind = np.transpose(ind)
+    return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
+
+def get_imgdata(file,sfile,re_size=16,n=5):
+    import pickle
+    import numpy as np
+    from skimage.transform import resize
+
+    def unpickle(file):
+        with open(file, 'rb') as f:
+            cifar_dict = pickle.load(f, encoding='latin1')
+        return cifar_dict
+    # 定义用来存放图像数据 图像标签 图像名称list  最后返回的cifar_image cifar_label即是图像cifar-10 对应的数据和标签
+    tem_cifar_image = []
+    tem_cifar_label = []
+    tem_cifar_image_name = []
+    for i in range(1, n+1):
+        # 存放是你的文件对应的目录
+        cifar_file = sfile + str(i)
+        cifar = unpickle(cifar_file)
+        cifar_label = cifar['labels']
+        cifar_image = cifar['data']
+        cifar_image_name = cifar['filenames']
+        # 使用transpose()函数是因为cifar存放的是图像标准是 通道数 高 宽 所以要修改成  高 宽 通道数
+        cifar_image = cifar_image.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).astype("float")
+        cifar_image = np.asarray([resize(x_img, [re_size, re_size]) for x_img in cifar_image])
+        cifar_label = np.array(cifar_label)
+        cifar_image_name = np.array(cifar_image_name)
+        tem_cifar_image.append(cifar_image)
+        tem_cifar_label.append(cifar_label)
+        tem_cifar_image_name.append(cifar_image_name)
+    cifar_image = np.concatenate(tem_cifar_image)
+    cifar_label = np.concatenate(tem_cifar_label)
+    cifar_image_name = np.concatenate(tem_cifar_image_name)
+    return cifar_image, cifar_label, cifar_image_name
+
+## 朴素贝叶斯与KNN分类
+def knn_gnb_lr_lsr(X, labels, title_knn="CIFAR.KNN",\
+                title_gnb="CIFAR.GNB",title_lr="CIFAR.LR",\
+                title_lsr="CIFAR.LSR",n = 3):
+    # 划分训练集和测试集
+    x_train, x_test, labels_train, labels_test =\
+        train_test_split(X, labels, test_size=0.2, random_state=22)
+
+    # 使用KNN进行分类
+    knn = KNeighborsClassifier()
+    knn.fit(x_train, labels_train)
+    label_sample = knn.predict(x_test)
+    knn_acc=cluster_acc(labels_test, label_sample)
+    print(title_knn,"=",knn_acc)
+
+    # 使用高斯朴素贝叶斯进行分类
+    gnb = GaussianNB()  # 使用默认配置初始化朴素贝叶斯
+    gnb.fit(x_train, labels_train)  # 训练模型
+    label_sample = gnb.predict(x_test)
+    gnb_acc = cluster_acc(labels_test, label_sample)
+    print(title_gnb,"=", gnb_acc)
+
+    # 线性回归
+    lr = LinearRegression()
+    lr.fit(x_train, labels_train)
+    label_sample = lr.predict(x_test)
+    label_sample = np.round(label_sample)
+    label_sample=label_sample.astype(np.int64)
+    lr_acc = cluster_acc(labels_test, label_sample)
+    print(title_lr, "=", lr_acc)
+
+    #Logistic regression 需要事先进行标准化
+    #创建一对多的逻辑回归对象
+    # 标准化特征
+    scaler = StandardScaler()
+    X_ = scaler.fit_transform(X,labels)
+    # 划分训练集和测试集
+    x_train, x_test, labels_train, labels_test = \
+        train_test_split(X_, labels, test_size=0.2)
+    log_reg = LogisticRegression(max_iter=3000)#multinomial
+    #训练模型
+    log_reg.fit(x_train, labels_train)
+    label_sample = log_reg.predict(x_test)
+    lsr_acc = cluster_acc(labels_test, label_sample)
+    print(title_lsr, "=", lsr_acc)
+    print('\n')
+
+    return round(knn_acc,n),round(gnb_acc,n),round(lr_acc,n),round(lsr_acc,n)
